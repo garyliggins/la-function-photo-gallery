d0344f7b7a87fa55571c7ea2c806abd3a0e557c9
gary <gary@ligginsdevelopment.com>
2022-04-22 21:37:50 -0700

    responsive image changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import images from './images.json';
 
 function App() {
    return (
-    <div className='container grid grid-cols-3 gap-4 mx-auto '>
+    <div className='container grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1   gap-4 mx-auto '>
        {images.map(image => ( 
       <img className='w-full rounded' src={image.src} alt={image.alt}/>
   ))}
