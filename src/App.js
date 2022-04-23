bff6cc8e34fde3acd498111c98b5242bda0ca887
gary <gary@ligginsdevelopment.com>
2022-04-22 21:08:14 -0700

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import images from './images.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   return (
+    <div className='container grid grid-cols-3 gap-4 mx-auto '>
+       {images.map(image => ( 
+      <img className='w-full rounded' src={image.src} alt={image.alt}/>
+  ))}
+      </div>
   );
+
 }
 
 export default App;
+
+  
