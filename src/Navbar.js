b8b224d8d85d26479b8355f5885aa821959fe43c
gary <gary@ligginsdevelopment.com>
2022-05-03 19:47:21 -0700

    added correct link to website

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
 
   return (
     <div className="sticky top-0 z-50 h-32 md:h-40 p-7 pt-10  md:px-20 bg-black fixed w-screen">
-     <a href="https://silver-sunflower-8f2de0.netlify.app" className="">
+     <a href="https://lafunction.com/" className="">
          <img className="float-left invert w-16 md:w-20 " src="LA-Function-Final-Logo copy.png" alt="LA Function Logo" ></img>
      </a>
      <div>
